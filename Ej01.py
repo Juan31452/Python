7ce178aeaae7867c8b255305bc1b6e3bb01476db
Juan31452 <jcs1011@gmail.com>
2021-11-02 20:42:10 -0500

    operaciones

---
+++ Ej01.py
@@ -0,0 +1,10 @@
+numero1 = 0
+numero2 = 0
+suma = 0
+resta = 0
+multiplica = 0
+divide = 0
+numero1 = int(input("Primer Numero :"))
+numero2 = int(input("Segundo Numero :"))
+suma = str(numero1 + numero2)
+print ("La suma es : ",suma)
