8952e8455a1350d8e9aa385fa4ef6e4b0e0ac89e
Juan31452 <jcs1011@gmail.com>
2021-11-03 20:17:36 -0500

    formula

---
+++ Ej05.py
@@ -0,0 +1,8 @@
+a = 0
+b = 0
+c = 0
+print("Se evalua la expresion(a+7*c)/(b+2-a)+2*b")
+a = int(input("Valor a :"))
+b = int(input("Valor b :"))
+c = int(input("Valor c :"))
+print((a+7*c)/(b+2-a)+2*b)
