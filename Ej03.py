aaff79d9147304072c853209b9d0e31bfa030b78
Juan31452 <jcs1011@gmail.com>
2021-11-03 19:40:52 -0500

    media

---
+++ Ej03.py
@@ -0,0 +1,9 @@
+numero1 = 0
+numero2 = 0
+numero3 = 0
+numero4 = 0
+numero1 = int(input("Primer Numero :"))
+numero2 = int(input("Segundo Numero :"))
+numero3 = int(input("Tercer Numero :"))
+numero4 = int(input("Cuarto Numero :"))
+print("La media de es :",(numero1 + numero2 + numero3 + numero4)/4)
