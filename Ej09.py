48177e28976cd209bfce3b440b2295f4579ffdac
Juan31452 <jcs1011@gmail.com>
2022-05-09 20:16:49 -0500

    listas y tuples

--- Ej09.py
+++ Ej09.py
@@ -5,7 +5,7 @@ def myfunc():
     
   print("La Lista es "+ str(Milista))
   print("El tuple es " + str(Mituple))
-  
+  Milista.append("Naranja")  
   for i in range(len(Milista)):
     print(str(Milista[i]))
   
