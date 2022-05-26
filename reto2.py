9cd7c21dd954c4ea6a220727bdc126a901a38d64
Juan31452 <jcs1011@gmail.com>
2022-05-25 19:49:15 -0500

    arreglo reto2

--- reto2.py
+++ reto2.py
@@ -5,30 +5,40 @@ def cliente(informacion:dict):
     informacion["edad"]
     informacion["primer_ingreso"]
     
-    if informacion["edad"] > 18 :
+    if informacion["edad"] > 18:
      informacion["atraccion"] = "X-treme"
-     valor_boleta =20000
-     descuento = 0.05
-     t_descuento = valor_boleta * descuento
-     total = valor_boleta - t_descuento
-     informacion["total_boleta"] = total
-     
+     informacion["apto"] = True
+     valor_boleta = 20000
+     if informacion['primer_ingreso'] == True:
+      descuento = 0.05
+      t_descuento = valor_boleta * descuento
+      total = valor_boleta - t_descuento
+      informacion["total_boleta"] = total
+     else:
+      informacion["total_boleta"] = valor_boleta
+              
     elif informacion["edad"] >= 15 and informacion["edad"]<= 18 :
      informacion["atraccion"] = "carros chocones"
      valor_boleta =5000
-     descuento = 0.07
-     t_descuento = valor_boleta * descuento
-     total = valor_boleta - t_descuento
-     informacion["total_boleta"] = total   
+     if informacion['primer_ingreso'] == True:
+      descuento = 0.07
+      t_descuento = valor_boleta * descuento
+      total = valor_boleta - t_descuento
+      informacion["total_boleta"] = total   
+     else:
+      informacion["total_boleta"] = valor_boleta
     
     elif informacion["edad"] >= 7 and informacion["edad"]< 15 :
      informacion["atraccion"] = "sillas_voladoras"
      valor_boleta =10000
-     descuento = 0.05
-     t_descuento = valor_boleta * descuento
-     total = valor_boleta - t_descuento
-     informacion["total_boleta"] = total   
-     
+     if informacion['primer_ingreso'] == True:
+      descuento = 0.05
+      t_descuento = valor_boleta * descuento
+      total = valor_boleta - t_descuento
+      informacion["total_boleta"] = total   
+     else:
+      informacion["total_boleta"] = valor_boleta
+    
     return informacion
 
 informacion={}
@@ -37,6 +47,7 @@ informacion["nombre"] = "Juan"
 informacion["edad"] = 8
 informacion["primer_ingreso"] = True
 print("Datos :", cliente(informacion))
+
 
     
     
