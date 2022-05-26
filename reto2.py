80dd98962dc2796c740834382a48c6ab51616fbf
Juan31452 <jcs1011@gmail.com>
2022-05-25 22:14:10 -0500

    arreglo reto2

--- reto2.py
+++ reto2.py
@@ -1,51 +1,63 @@
-def cliente(informacion:dict):
+def cliente(informacion:dict)->dict:
     
-    informacion["id_cliente"]
-    informacion["nombre"]
-    informacion["edad"]
-    informacion["primer_ingreso"]
+
+    respuesta={}
+    respuesta['nombre'] = informacion['nombre']
+    respuesta['edad'] = informacion['edad']
+    
     
     if informacion["edad"] > 18:
-     informacion["atraccion"] = "X-treme"
-     informacion["apto"] = True
-     valor_boleta = 20000
+     respuesta["atraccion"] = "X-treme"
+     respuesta["apto"] = True
      if informacion['primer_ingreso'] == True:
+      valor_boleta = 20000
       descuento = 0.05
       t_descuento = valor_boleta * descuento
       total = valor_boleta - t_descuento
-      informacion["total_boleta"] = total
+      respuesta['primer_ingreso'] = True
+      respuesta["total_boleta"] = total
      else:
-      informacion["total_boleta"] = valor_boleta
+      respuesta['primer_ingreso'] = False
+      respuesta["total_boleta"] = valor_boleta
               
     elif informacion["edad"] >= 15 and informacion["edad"]<= 18 :
-     informacion["atraccion"] = "carros chocones"
-     valor_boleta =5000
+     respuesta["atraccion"] = "carros chocones"
+     respuesta["apto"] = True
      if informacion['primer_ingreso'] == True:
+      valor_boleta =5000
       descuento = 0.07
       t_descuento = valor_boleta * descuento
       total = valor_boleta - t_descuento
-      informacion["total_boleta"] = total   
+      respuesta['primer_ingreso'] = True
+      respuesta["total_boleta"] = total   
      else:
-      informacion["total_boleta"] = valor_boleta
+      respuesta['primer_ingreso'] = False
+      respuesta["total_boleta"] = valor_boleta
     
     elif informacion["edad"] >= 7 and informacion["edad"]< 15 :
-     informacion["atraccion"] = "sillas_voladoras"
-     valor_boleta =10000
+     respuesta["atraccion"] = "sillas_voladoras"
+     respuesta["apto"] = True
      if informacion['primer_ingreso'] == True:
+      valor_boleta =10000
       descuento = 0.05
       t_descuento = valor_boleta * descuento
       total = valor_boleta - t_descuento
+      respuesta['primer_ingreso'] = True
       informacion["total_boleta"] = total   
      else:
+      respuesta['primer_ingreso'] = False
       informacion["total_boleta"] = valor_boleta
     
-    return informacion
+    elif informacion["edad"] < 7:
+     informacion["atraccion"] = "N/A"
+     informacion["apto"] = False
+     informacion["total_boleta"] = "N/A"   
+         
+    return respuesta
 
 informacion={}
-informacion["id_cliente"] = 1
-informacion["nombre"] = "Juan"
-informacion["edad"] = 8
-informacion["primer_ingreso"] = True
+informacion={"Id_cliente":1,"nombre":"Jhoana Fernandez","edad":20,"primer_ingreso":True}
+
 print("Datos :", cliente(informacion))
 
 
