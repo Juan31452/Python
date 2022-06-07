9dfacf5cf4ffddcbd9ce7dfb5f64cef33dc0b77d
Juan31452 <jcs1011@gmail.com>
2022-06-06 19:16:15 -0500

    corregido error reto3

--- reto3.py
+++ reto3.py
@@ -22,13 +22,13 @@ consultaRegistro(AutoPartes([
 (3578,'tijera', 'QW8523',1,128,'Pedro Faria',1456,'12/06/2020'),
 (9251,'piñón', 'EN5698',2,8,'Juan Peña',565,'12/06/2020')]), 2010)
 print()
-consultaRegistro(Autopartes([
+consultaRegistro(AutoPartes([
 (5489,'tornillo', 'RS8512',2,33,'Julio Perez',3654213,'13/06/2020'),
 (3215,'zocalo', 'UM8587',2,125,'Laura Macias',1256321,'13/06/2020'),
 (3698,'biela', 'PT3218',1,78,'Luis Peña',14565487,'13/06/2020'),
 (8795,'cilindro', 'AZ8794',2,96,'Carlos Casio',5612405,'13/06/2020')]), 2001)
 print()
-consultaRegistro(Autopartes([
+consultaRegistro(AutoPartes([
 (9852,'Culata', 'XC9875',2,165,'Luis Molero',3455846,'14/06/2020'),
 (9852,'Culata', 'XC9875',2,165,'Jose Mejia',1355846,'14/06/2020'),
 (2564,'Cárter', 'PT29872',2,32,'Peter Cerezo',8545436,'14/06/2020'),
